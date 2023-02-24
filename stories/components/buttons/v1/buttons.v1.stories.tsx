6ef46e957e04f9c96ab50f23554ffef97ded39a4
Alef Santos Soares <54686408+Olafi-MooN@users.noreply.github.com>
2023-02-24 18:45:26 -0300

    Feat: Config initial project

--- stories/components/buttons/v1/buttons.v1.stories.tsx
+++ stories/components/buttons/v1/buttons.v1.stories.tsx
@@ -1,4 +1,4 @@
-import React, { ButtonHTMLAttributes } from "react";
+import React from "react";
 import { Meta, Story } from "@storybook/react";
 import { AllButton } from "../../../../src/a-all/components/buttons/v1/button.v1";
 import { IAllButtonV1Props } from "../../../../src/a-all/components/buttons/v1/button.v1.interface";
