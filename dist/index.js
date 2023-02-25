c0dc4478d1a4dfbca0b12a542eb0ae20c6abc74e
Alef Santos Soares <54686408+Olafi-MooN@users.noreply.github.com>
2023-02-24 23:59:04 -0300

    theme color

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root") as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <h1>Lib A-All</h1>
+//   </React.StrictMode>
+// );
+export * from "./a-all";
