9846b570e79303dfdddfc962ff4f462a64694db7
Alef Santos Soares <54686408+Olafi-MooN@users.noreply.github.com>
2023-02-25 22:56:45 -0300

    remove dist

--- dist/a-all/components/buttons/index.js
+++
@@ -1,2 +0,0 @@
-import "../../../colors.css";
-export * from "./v1";
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-// const root = ReactDOM.createRoot(
-//   document.getElementById("root") as HTMLElement
-// );
-// root.render(
-//   <React.StrictMode>
-//     <h1>Lib A-All</h1>
-//   </React.StrictMode>
-// );
-export * from "./a-all";
